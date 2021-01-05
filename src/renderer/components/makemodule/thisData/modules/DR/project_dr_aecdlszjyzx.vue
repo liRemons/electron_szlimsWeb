80b476899341fffb22259fba0fd40cdfe9ad18e8
xds <1454894181@qq.com>
2021-01-05 11:56:38 +0800

    udpate DR

--- src/renderer/components/makemodule/thisData/modules/DR/project_dr_aecdlszjyzx.vue
+++ src/renderer/components/makemodule/thisData/modules/DR/project_dr_aecdlszjyzx.vue
@@ -1,127 +1,178 @@
 <template>
-    <div style="line-height: 30px; padding-top: 10px;" class="___relative">
-        <div :class="{eventCover:!ableInput}"></div>
-        <div>
-            <div style="text-align: center;">
-                <div style="border: 1px solid black;font-weight: bolder; text-align: left; padding-left: 15px;">
-                    AEC电离室之间一致性（{{data.valueData.title}}）
-                </div>
-                <div style="border: 1px solid black;font-weight: bolder; border-top: none; line-height: 20px;">
-                    标准要求
-                </div>
-                <div  class="tl" style="border: 1px solid black; border-top: none;">
-                    验收：±10.0%内；状态：±15.0%内。
-                </div>
-                <div style="border: 1px solid black;font-weight: bolder; border-top: none;">
-                    检测及计算方法
-                </div>
-                <div class="tl" style="border: 1px solid black; border-top: none; padding: 5px; line-height: 20px;">
-                    70kV，1mmCu滤过，分别选择一个电离室，在AEC条件下曝光，记录mA，s或mAs或DDI值，将每两个电离室的测量值X进行比较，计算平均值最大偏差。电离室i测量值Xi与电离室j测量值Xj之间的平均值偏差ij=(Xi-Xj)/(Xi+Xj)。
-                </div>
-                <div style="border: 1px solid black;font-weight: bolder; border-top: none;">
-                    检测结果
-                </div>
-            </div>
-            <table class="myTable">
-                <tr class="delLine">
-                    <td style="width: 230px;">检测指标</td>
-                    <td>电离室1</td>
-                    <td>电离室2</td>
-                    <td>电离室3</td>
-                    <td style="width: 90px;">平均值偏差<sub>12</sub></td>
-                    <td style="width: 90px;">检测结果<sub>23</sub></td>
-                    <td style="width: 90px;">平均值偏差<sub>13</sub></td>
-                </tr>
-                <tr>
-                    <td>
-                        <el-radio label="mA、s" style="width: 30px;"
-                                  @click.native.prevent="computeObj.clearRadio('mA、s', data.valueData, 'radioValue')"
-                                  v-model="data.valueData.radioValue"></el-radio>
-                        <el-radio label="mAs" style="width: 30px;"
-                                  @click.native.prevent="computeObj.clearRadio('mAs', data.valueData, 'radioValue')"
-                                  v-model="data.valueData.radioValue"></el-radio>
-                        <el-radio label="DDI值" style="width: 30px;"
-                                  @click.native.prevent="computeObj.clearRadio('DDI值', data.valueData, 'radioValue')"
-                                  v-model="data.valueData.radioValue"></el-radio>
-                    </td>
-                    <td>
-                        <myInput v-model="data.valueData.arr[0]" @change.native="control"></myInput>
-                    </td>
-                    <td>
-                        <myInput v-model="data.valueData.arr[1]" @change.native="control"></myInput>
-                    </td>
-                    <td>
-                        <myInput v-model="data.valueData.arr[2]" @change.native="control"></myInput>
-                    </td>
-                    <td>
-                        {{data.valueData.arr[3]}}
-                    </td>
-                    <td>
-                        {{data.valueData.arr[4]}}
-                    </td>
-                    <td>
-                        {{data.valueData.arr[5]}}
-                    </td>
-                </tr>
-            </table>
-            <div class="___absolute myBtn" style="left: 750px; top: 0px; width: 65px; text-align: center;" @click="sure" v-if="target ==='0'">
-                完成
-            </div>
+  <div style="line-height: 30px; padding-top: 10px;" class="___relative">
+    <div :class="{ eventCover: !ableInput }"></div>
+    <div>
+      <div style="text-align: center;">
+        <div
+          style="border: 1px solid black;font-weight: bolder; text-align: left; padding-left: 15px;"
+        >
+          AEC电离室之间一致性（{{ data.valueData.title }}）
         </div>
+        <div
+          style="border: 1px solid black;font-weight: bolder; border-top: none; line-height: 20px;"
+        >
+          标准要求
+        </div>
+        <div class="tl" style="border: 1px solid black; border-top: none;">
+          验收：±10.0%内；状态：±15.0%内。
+        </div>
+        <div
+          style="border: 1px solid black;font-weight: bolder; border-top: none;"
+        >
+          检测及计算方法
+        </div>
+        <div
+          class="tl"
+          style="border: 1px solid black; border-top: none; padding: 5px; line-height: 20px;"
+        >
+          70kV，1mmCu滤过，分别选择一个电离室，在AEC条件下曝光，记录mA，s或mAs或DDI值，将每两个电离室的测量值X进行比较，计算平均值最大偏差。电离室i测量值Xi与电离室j测量值Xj之间的平均值偏差ij=(Xi-Xj)/(Xi+Xj)。
+        </div>
+        <div
+          style="border: 1px solid black;font-weight: bolder; border-top: none;"
+        >
+          检测结果
+        </div>
+      </div>
+      <table class="myTable">
+        <tr class="delLine">
+          <td style="width: 230px;">检测指标</td>
+          <td>电离室1</td>
+          <td>电离室2</td>
+          <td>电离室3</td>
+          <td style="width: 90px;">平均值偏差<sub>12</sub></td>
+          <td style="width: 90px;">平均值偏差<sub>23</sub></td>
+          <td style="width: 90px;">平均值偏差<sub>13</sub></td>
+        </tr>
+        <tr>
+          <td>
+            <el-radio
+              label="mA、s"
+              style="width: 30px;"
+              @click.native.prevent="
+                computeObj.clearRadio('mA、s', data.valueData, 'radioValue')
+              "
+              v-model="data.valueData.radioValue"
+            ></el-radio>
+            <el-radio
+              label="mAs"
+              style="width: 30px;"
+              @click.native.prevent="
+                computeObj.clearRadio('mAs', data.valueData, 'radioValue')
+              "
+              v-model="data.valueData.radioValue"
+            ></el-radio>
+            <el-radio
+              label="DDI值"
+              style="width: 30px;"
+              @click.native.prevent="
+                computeObj.clearRadio('DDI值', data.valueData, 'radioValue')
+              "
+              v-model="data.valueData.radioValue"
+            ></el-radio>
+          </td>
+          <td>
+            <myInput
+              v-model="data.valueData.arr[0]"
+              @change.native="control"
+            ></myInput>
+          </td>
+          <td>
+            <myInput
+              v-model="data.valueData.arr[1]"
+              @change.native="control"
+            ></myInput>
+          </td>
+          <td>
+            <myInput
+              v-model="data.valueData.arr[2]"
+              @change.native="control"
+            ></myInput>
+          </td>
+          <td>
+            {{ data.valueData.arr[3] }}
+          </td>
+          <td>
+            {{ data.valueData.arr[4] }}
+          </td>
+          <td>
+            {{ data.valueData.arr[5] }}
+          </td>
+        </tr>
+      </table>
+
+      <div
+        class="___absolute myBtn"
+        style="left: 750px; top: 0px; width: 65px; text-align: center;"
+        @click="sure"
+        v-if="target === '0'"
+      >
+        完成
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "project_dr_aecdlszjyzx",
-        props: ['data',"ableInput", 'jsonString', 'target'],
-        computed: {
-            state () {
-                return this.computeObj.judgeState(this.jsonString.find(item => item.to === 'project_jbxx').data.valueData.purposeDetection);
-            },
-            A() {
-                return Number(this.data.valueData.arr[0]);
-            },
-            B() {
-                return Number(this.data.valueData.arr[1]);
-            },
-            C() {
-                return Number(this.data.valueData.arr[2]);
-            }
-        },
-        methods: {
-            control(){
-                this.gs1();
-                this.gs2();
-                this.gs3();
-                this.$forceUpdate();
-            },
-            gs1(){
-                this.data.valueData.arr[3] = ((this.A - this.B) / (this.A + this.B)).toFixed(2);
-            },
-            gs2(){
-                this.data.valueData.arr[4] = ((this.B - this.C) / (this.B + this.C)).toFixed(2);
-            },
-            gs3(){
-                this.data.valueData.arr[5] = ((this.A - this.C) / (this.A + this.C)).toFixed(2);
-            },
-            sure(){
-                if(this.data.valueData.radioValue === ""){
-                    this.$notify({
-                        type: 'warning',
-                        message: '检测指标必须选一个'
-                    });
-                } else {
-                    this.$notify({
-                        type: 'success',
-                        message: '已完成'
-                    });
-                }
-            }
-        }
-    }
+export default {
+  name: 'project_dr_aecdlszjyzx',
+  props: ['data', 'ableInput', 'jsonString', 'target'],
+  computed: {
+    state() {
+      return this.computeObj.judgeState(
+        this.jsonString.find((item) => item.to === 'project_jbxx').data
+          .valueData.purposeDetection
+      )
+    },
+    A() {
+      return Number(this.data.valueData.arr[0])
+    },
+    B() {
+      return Number(this.data.valueData.arr[1])
+    },
+    C() {
+      return Number(this.data.valueData.arr[2])
+    },
+  },
+  methods: {
+    control() {
+      this.gs1()
+      this.gs2()
+      this.gs3()
+      this.$forceUpdate()
+    },
+    gs1() {
+      this.data.valueData.arr[3] = (
+        (this.A - this.B) /
+        (this.A + this.B)
+      ).toFixed(2)
+    },
+    gs2() {
+      this.data.valueData.arr[4] = (
+        (this.B - this.C) /
+        (this.B + this.C)
+      ).toFixed(2)
+    },
+    gs3() {
+      this.data.valueData.arr[5] = (
+        (this.A - this.C) /
+        (this.A + this.C)
+      ).toFixed(2)
+    },
+    sure() {
+      if (this.data.valueData.radioValue === '') {
+        this.$notify({
+          type: 'warning',
+          message: '检测指标必须选一个',
+        })
+      } else {
+        this.$notify({
+          type: 'success',
+          message: '已完成',
+        })
+      }
+    },
+  },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/renderer/components/makemodule/thisData/modules/DR/project_dr_aeclmd.vue
+++ src/renderer/components/makemodule/thisData/modules/DR/project_dr_aeclmd.vue
@@ -1,12 +1,12 @@
 <template>
   <div style="line-height: 30px; padding-top: 10px;" class="___relative">
-    <div :class="{ eventCover:!ableInput }"></div>
+    <div :class="{ eventCover: !ableInput }"></div>
     <div>
       <div style="text-align: center;">
         <div
           style="border: 1px solid black;font-weight: bolder; text-align: left; padding-left: 15px;"
         >
-          AEC灵敏度（{{data.valueData.title}}）
+          AEC灵敏度（{{ data.valueData.title }}）
         </div>
         <div
           style="border: 1px solid black;font-weight: bolder; border-top: none; line-height: 20px;"
@@ -22,7 +22,7 @@
           检测及计算方法
         </div>
         <div
-        class="tl"
+          class="tl"
           style="border: 1px solid black; border-top: none; padding: 5px; line-height: 20px;"
         >
           70kV，1mmCu滤过，选择所有的电离室，在AEC条件下曝光，记录mA，s，或mAs或DDI值。偏差=（检测结果-基线值）/基线值×100%。
@@ -34,14 +34,18 @@
         </div>
       </div>
       <table class="myTable">
-        <tr class="delLine">
+        <tr class="delLine" style="line-height:16px">
           <td>SID/cm</td>
-          <td style="width: 100px;">滤线栅是否取出</td>
-          <td>器官曝光程序</td>
+          <td>滤线栅<br />是否取出</td>
+          <td>是否有<br />床面衰减</td>
+          <td>器官曝光<br />程序</td>
           <td>附加滤过</td>
+          <td>AEC密度</td>
+          <td>AEC<br />感光度</td>
+          <td>打开电<br />离室数量</td>
           <td>检测指标</td>
           <td>检测结果</td>
-          <td style="text-align: left; padding-left: 5px; width: 105px;">
+          <td style="text-align: left; padding-left: 5px; width: 60px;">
             <p v-if="state === '状态检测'">
               <el-radio
                 v-model="data.valueData.radioVal"
@@ -53,6 +57,7 @@
             </p>
             <p>
               <el-radio
+                style="white-space: normal !important"
                 v-model="data.valueData.radioVal"
                 @click.native.prevent="
                   computeObj.clearRadio(
@@ -87,6 +92,21 @@
             </selectModel>
           </td>
           <td>
+            <selectModel
+              @returnVal="returnVal"
+              :Judge="true"
+              :special="2"
+              :multi-select="false"
+              :receive="'AECNum'"
+              :transmitText="data.valueData.arr2[6]"
+              :single="true"
+              :rows="false"
+              :list="['是', '否']"
+              :Obj="''"
+            >
+            </selectModel>
+          </td>
+          <td>
             <myInput v-model="data.valueData.arr2[2]"></myInput>
           </td>
           <td>
@@ -94,8 +114,11 @@
             <br />
             mmCu
           </td>
+          <td><myInput v-model="data.valueData.arr2[7]"></myInput></td>
+          <td><myInput v-model="data.valueData.arr2[8]"></myInput></td>
+          <td><myInput v-model="data.valueData.arr2[9]"></myInput></td>
           <td style="text-align: left; padding-left: 5px;">
-            <p>
+            <!-- <p>
               <el-radio
                 v-model="data.valueData.baseValue"
                 @click.native.prevent="
@@ -103,7 +126,7 @@
                 "
                 label="mA、s"
               ></el-radio>
-            </p>
+            </p> -->
             <p>
               <el-radio
                 v-model="data.valueData.baseValue"
@@ -127,8 +150,8 @@
             <myInput
               v-model="data.valueData.arr2[3]"
               @keyup.native="
-                el => {
-                  setEValue(el, 0);
+                (el) => {
+                  setEValue(el, 0)
                 }
               "
             ></myInput>
@@ -137,8 +160,8 @@
             <myInput
               v-model="data.valueData.arr2[4]"
               @keyup.native="
-                el => {
-                  setEValue(el, 1);
+                (el) => {
+                  setEValue(el, 1)
                 }
               "
             ></myInput>
@@ -201,81 +224,80 @@
 
 <script>
 export default {
-  name: "project_dr_aeclmd",
-  props: ["data","ableInput", "jsonString", "target"],
+  name: 'project_dr_aeclmd',
+  props: ['data', 'ableInput', 'jsonString', 'target'],
   computed: {
     state() {
       return this.computeObj.judgeState(
-        this.jsonString.find(item => item.to === "project_jbxx").data.valueData
-          .purposeDetection
-      );
-    }
+        this.jsonString.find((item) => item.to === 'project_jbxx').data
+          .valueData.purposeDetection
+      )
+    },
   },
   watch: {
-    "data.valueData.radioVal": function(val) {
-      if (val === "建立基线值") {
-        this.data.valueData.arr2[5] = "";
-      } else if (val === "基线值") {
-        this.data.valueData.note = "";
+    'data.valueData.radioVal': function(val) {
+      if (val === '建立基线值') {
+        this.data.valueData.arr2[5] = ''
+      } else if (val === '基线值') {
+        this.data.valueData.note = ''
       }
-      this.setEValue("", 2);
-    }
+      this.setEValue('', 2)
+    },
   },
   methods: {
     returnVal(nowVal) {
-      this.data.valueData.arr2[1] = nowVal;
+      this.data.valueData.arr2[1] = nowVal
     },
     sure() {
-      if (this.data.valueData.baseValue === "") {
+      if (this.data.valueData.baseValue === '') {
         this.$notify({
-          type: "warning",
-          message: "检测指标 三个必须选择一个"
-        });
+          type: 'warning',
+          message: '检测指标 三个必须选择一个',
+        })
       } else {
         if (
-          this.state === "状态检测" &&
-          this.data.valueData.radioVal === "建立基线值" &&
-          this.data.valueData.note === ""
+          this.state === '状态检测' &&
+          this.data.valueData.radioVal === '建立基线值' &&
+          this.data.valueData.note === ''
         ) {
           this.$notify({
-            type: "warning",
-            message: "备注必须二选一"
-          });
+            type: 'warning',
+            message: '备注必须二选一',
+          })
         } else {
           this.$notify({
-            type: "success",
-            message: "已完成"
-          });
+            type: 'success',
+            message: '已完成',
+          })
         }
       }
     },
     setEValue(el, flag) {
-      if (this.data.valueData.radioVal === "基线值") {
+      if (this.data.valueData.radioVal === '基线值') {
         try {
-          let c = "";
-          let d = "";
+          let c = ''
+          let d = ''
           if (flag === 0) {
-            c = el.target.value;
-            d = this.data.valueData.arr2[4];
+            c = el.target.value
+            d = this.data.valueData.arr2[4]
           } else if (flag === 1) {
-            c = this.data.valueData.arr2[3];
-            d = el.target.value;
+            c = this.data.valueData.arr2[3]
+            d = el.target.value
           } else if (flag === 2) {
-            c = this.data.valueData.arr2[3];
-            d = this.data.valueData.arr2[4];
+            c = this.data.valueData.arr2[3]
+            d = this.data.valueData.arr2[4]
           }
-          c = Number(c);
-          d = Number(d);
-          let e = ((c - d) * 100) / d;
-          this.data.valueData.arr2[5] = e.toFixed(2);
+          c = Number(c)
+          d = Number(d)
+          let e = ((c - d) * 100) / d
+          this.data.valueData.arr2[5] = e.toFixed(2)
         } catch (e) {
-          this.data.valueData.arr2[5] = "";
+          this.data.valueData.arr2[5] = ''
         }
       }
-    }
-  }
-};
+    },
+  },
+}
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
